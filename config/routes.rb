482533281cc3fa135873d7cfe0bfeef7d475a8d5
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-20 12:17:54 +0900

    add_model-relationship add_follower,followerd-count

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root to: 'home#top'
   get 'home/about' => "home#about"
   devise_for :users
-  resources :users, only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+  	member do
+  		get :following, :followers
+  	end
+  end
+
   resources :books do
   	resources :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
