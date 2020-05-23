a16319c3983bd346c7239494331a3bde2badf91f
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-05-23 17:42:37 +0900

    add_userindex-selecltag-retrieval method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,6 @@ class UsersController < ApplicationController
       if params[:name].present?
          @users = User.where('name LIKE ?', "%#{params[:name]}%")
          @user = current_user
-         # @users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
          @book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
          # redirect_to user_search_users_path
       else
